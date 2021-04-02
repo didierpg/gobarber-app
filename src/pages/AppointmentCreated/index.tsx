69af21e975f6edf0f9954ff6d233436341c3375b
Didier <didierpg@gmail.com>
2021-04-01 23:40:54 -0300

    Finalizando front-end mobile do app ▸ Agendamento ▸ Criando estrutura da tela

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
-import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
+import React, { useCallback } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import {
+  Container,
+  Description,
+  OkButton,
+  OkButtonText,
+  Title,
+} from './styles';
 
 const AppointmentCreated: React.FC = () => {
-  return <View />;
+  const { reset } = useNavigation();
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [
+        {
+          name: 'Dashboard',
+        },
+      ],
+      index: 0,
+    });
+  }, [reset]);
+
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento concluído</Title>
+      <Description>Terça, dia 14 de março de 2020 às 12:00h</Description>
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>Ok</OkButtonText>
+      </OkButton>
+    </Container>
+  );
 };
 
 export default AppointmentCreated;
